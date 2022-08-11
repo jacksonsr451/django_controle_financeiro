a3608ebe5f03d2054c810f4009e881ea6ee5ac8d
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-08-11 13:46:05 -0300

    Implementing models

--- despesas/models.py
+++ despesas/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Despesas(models.Model):
+    id = models.AutoField(primary_key=True)
+    descricao = models.TextField(null=False)
+    valor = models.FloatField(null=False)
+    data = models.DateTimeField(null=False)
+    
+    
+    class Meta:
+        ordering = ['id', 'descricao', 'valor', 'data']
+    
+    
+    def __init__(self, descricao=None, valor=None, data=None, *args, **kwargs) -> None:
+        self.descricao = descricao
+        self.valor = valor
+        self.data = data
+        
+    
