b0715d0f71511376b461aae493194b69a3922821
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-08-12 19:46:57 -0300

    Refactoring: add unique_for_month="data" in descricao model Despesas

--- despesas/views.py
+++ despesas/views.py
@@ -22,18 +22,8 @@ class Despesas(APIView):
 
     def post(self, request) -> Response:
         serializer = DespesaSerializer(data=request.data)
-        if serializer.is_valid() and self.validate_post(request.data):
+        if serializer.is_valid():
             serializer.save()
             return Response(status=status.HTTP_200_OK, data={"message": "Dados inseridos com sucesso!"})
         return Response(status=status.HTTP_400_BAD_REQUEST, data={"error": "Dados não inseridos com sucesso!"})
 
-    
-    def validate_post(self, request) -> bool:
-        despesas = DespesasModel.objects.all()
-        for despesa in despesas:
-            data_atual = despesa.data.__str__().split('-')
-            if despesa.descricao.__eq__(request["descricao"]):
-                if data_atual[1].__eq__(request["data"].split('-')[1])  \
-                    and data_atual[0].__eq__(request["data"].split('-')[0]):
-                    return False
-        return True
