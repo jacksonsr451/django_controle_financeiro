51dc54f04728f0703ec10d9318d13e84e9e57690
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-08-13 11:40:13 -0300

    Implementing: resumo

--- resumo/tests.py
+++ resumo/tests.py
@@ -1,3 +1,87 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+
+class TestResumo(APITestCase):
+    URL = "/api/v1/resumo/"
+    
+    def test_should_be_return_values_by_ano_and_mes(self):
+        self.insert_data()
+        value = self.get_value()
+        response = self.client.get(self.URL + "2022/08", format='json')
+        self.assertEqual(value, response.data)
+    
+    
+    def get_value(self):
+        return {
+            "total receitas": 1200.0,
+            "total despesas": 1200.0,
+            "saldo final": 0.0,
+            "total gasto por categoria": {
+                "Alimentação": 400.0,
+                "Saúde": 400.0,
+                "Moradia": 0.0,
+                "Transporte": 0.0,
+                "Educação": 400.0,
+                "Lazer": 0.0,
+                "Imprevistos": 0.0,
+                "Outras": 0.0
+            }
+        }
+    
+    
+    def insert_data(self):
+        self.client.post("/api/v1/despesas/", data={
+            "categoria": "Alimentação", "descricao": "Primeira despesa", "valor": 200.00, "data": "2022-08-12 17:20:55"
+        })
+        self.client.post("/api/v1/receitas/", data={
+            "descricao": "Primeira receita", "valor": 200.00, "data": "2022-08-12 17:20:55"
+        })
+        self.client.post("/api/v1/despesas/", data={
+            "categoria": "Alimentação", "descricao": "Segunda despesa", "valor": 200.00, "data": "2022-08-12 17:20:55"
+        })
+        self.client.post("/api/v1/receitas/", data={
+            "descricao": "Segunda receita", "valor": 200.00, "data": "2022-08-12 17:20:55"
+        })
+        self.client.post("/api/v1/despesas/", data={
+            "categoria": "Saúde", "descricao": "Terceira despesa", "valor": 200.00, "data": "2022-08-12 17:20:55"
+        })
+        self.client.post("/api/v1/receitas/", data={
+            "descricao": "Terceita receita", "valor": 200.00, "data": "2022-08-12 17:20:55"
+        })
+        self.client.post("/api/v1/despesas/", data={
+            "categoria": "Saúde", "descricao": "Quarta despesa", "valor": 200.00, "data": "2022-08-12 17:20:55"
+        })
+        self.client.post("/api/v1/receitas/", data={
+            "descricao": "Quarta receita", "valor": 200.00, "data": "2022-08-12 17:20:55"
+        })
+        self.client.post("/api/v1/despesas/", data={
+            "categoria": "Educação", "descricao": "Quinta despesa", "valor": 200.00, "data": "2022-08-12 17:20:55"
+        })
+        self.client.post("/api/v1/receitas/", data={
+            "descricao": "Quinta receita", "valor": 200.00, "data": "2022-08-12 17:20:55"
+        })
+        self.client.post("/api/v1/despesas/", data={
+            "categoria": "Educação", "descricao": "Sexta despesa", "valor": 200.00, "data": "2022-08-12 17:20:55"
+        })
+        self.client.post("/api/v1/receitas/", data={
+            "descricao": "Sexta receita", "valor": 200.00, "data": "2022-08-12 17:20:55"
+        })
+    
+    
+    def get_data(self) -> dict:
+        return {
+            "total receitas": 1200.00,
+            "total despesas": 1200.00,
+            "saldo final": 0.00,
+            "total gasto por categoria": {
+                "Alimentação": 400.00,
+                "Saúde": 400.00,
+                "Moradia": 0.00,
+                "Transporte": 0.00,
+                "Educação": 400.00,
+                "Lazer": 0.00,
+                "Imprevistos": 0.00,
+                "Outras": 0.00
+            }
+        }
